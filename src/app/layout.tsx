d35cb42b21165a9e1adc8821d85e2b192e75f1d9
theo-hrd <theo.hrd02@gmail.com>
2025-02-26 01:33:06 +0100

    Add Vercel Analytics to track website usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { Analytics } from "@vercel/analytics/next"
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={`${montserrat.variable} font-sans`}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
