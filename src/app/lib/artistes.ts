8009206f88168ac21517d5d5a1a1dd73e9f6e67e
theo-hrd <theo.hrd02@gmail.com>
2025-05-18 22:23:14 +0200

    Add new artist "LARSEN" and update existing artist details in artistesData; include new image for Akae and adjust performance times for several artists.

--- src/app/lib/artistes.ts
+++ src/app/lib/artistes.ts
@@ -11,48 +11,21 @@ export const artistesData = {
       },
       {
         id: 2,
-        nom: "Yoga Teacher", 
-        photo: '/artists/yogateacher.webp',
-        genre: 'Cardio Psych Rock',
-        heure: '20h00 - 21h30',
-        origine: 'Bruxelles - Belgique',
-        description: `Situé dans les prémices de la sortie de leurs premiers enregistrements, le groupe bruxellois Yoga Teacher façonne un son que l’on pourrait qualifier d’indie psychédélique groovy et chargé en riffs — à la fois accrocheur et distordu — influencé par les parcours musicaux propres à chacun des quatre membres, ainsi que par leur passion commune pour un rock sans compromis.`
-      },
-      {
-        id: 3,
         nom: "Till'", 
         photo: '/artists/till.webp',
         genre: 'Stoner Rock | Jazz | Groove',
-        heure: '21h30 - 22h30',
+        heure: '20h30',
         origine: 'Poitiers - France',
         description: `2022, Tom Ranson et Baudouin Salembier ont formé le groupe musical Till', aujourd'hui composé d'un guitariste, d'un saxophoniste, d'un bassiste et d'un batteur.
                       L'arrivée de Pierre-Antoine Clion, bassiste du groupe, a permis de réunir trois membres ayant une longue histoire commune, mais n'ayant jamais exploré leur connexion musicale en tant que trio En octobre 2023, lors du bal du chahut organisé par l'association Warp Unchained, le saxophoniste Hicham El Mansouri s'est joint au groupe pour jouer sur leur set.
                       Cette collaboration a enrichi de façon significative la musique du groupe, en apportant des mélodies envoûtantes se mêlant au jeu de guitare de Tom et à la rythmique complexe jouée par Baudouin et Pierre-Antoine. Leur rencontre a été une évidence, contribuant ainsi à créer une grande osmose musicale.`
       },
       {
-        id: 4,
-        nom: 'TRIFOUILLE1ER',
-        photo: '/artists/Trifouille1er.webp', 
-        genre: 'Musique électronique modulaire',
-        heure: '23h00 - 00h00',
-        origine: '',
-        description: `Fils et filtres, Radios et magnétos, Grande fêtes et tintamarre, Turbulences électroniques, Insouciance majestueuse, Antennes et potentiomètres, Modulations aléatoires, Commandes rectilignes, Hommes-pumas et bottes magiques, Générateur d'agréable, Télécommandes et parasites, Saturations et décodeurs, Grande Maison du Strobe, Laboratoire de séquences, Oscilloscopes et ondulations, Fantômes et trains de nuit.`
-      },
-      {
-        id: 5,
-        nom: 'PRPHTXIV & TSHIMBOMBO', 
-        photo: '/artists/julien.webp',
-        genre: 'House | Dub | Techno',
-        heure: '00h30 - 01h30',
-        origine: 'Poitiers - France',
-        description: ``
-      },
-      {
-        id: 6,
+        id: 3,
         nom: 'LARSEN',
         photo: '/artists/larsen.webp',
         genre: 'Techno | Hard Techno',
-        heure: '02h30 - 04h00',
+        heure: '22h00',
         origine: 'Poitiers - France',
         description: `LARSEN est le président et fondateur du collectif Badmood, aujourd'hui reconnu comme un acteur majeur de la scène Techno et Hard Music locale.
                       DJ passionné et engagé, il se distingue par une approche sans concession du mix, où l'éclectisme est poussé à l'outrance.
@@ -61,6 +34,34 @@ export const artistesData = {
                       Amateur de contrastes et de chaos maîtrisé, il cultive l'art de la surprise et de la tension, créant des montagnes russes sonores entre intensité brute et clins d'œil malicieux.
                       Fort d'une solide expérience en tant que DJ généraliste, il maîtrise les dynamiques de dancefloor sur le bout des doigts, ce qui lui permet d'embarquer tout type de public, du plus pointu au plus festif.`
       },
+      {
+        id: 4,
+        nom: 'TRIFOUILLE1ER',
+        photo: '/artists/Trifouille1er.webp', 
+        genre: 'Musique électronique modulaire',
+        heure: '23h30',
+        origine: '',
+        description: `Fils et filtres, Radios et magnétos, Grande fêtes et tintamarre, Turbulences électroniques, Insouciance majestueuse, Antennes et potentiomètres, Modulations aléatoires, Commandes rectilignes, Hommes-pumas et bottes magiques, Générateur d'agréable, Télécommandes et parasites, Saturations et décodeurs, Grande Maison du Strobe, Laboratoire de séquences, Oscilloscopes et ondulations, Fantômes et trains de nuit.`
+      },
+      {
+        id: 5,
+        nom: "TOOFOULK",
+        photo: '/artists/toofoulk.webp',
+        genre: 'Jungle | Drum and Bass | Neuro',
+        heure: '01h00',
+        origine: 'Poitiers - France',
+        description: ``
+      },
+      {
+        id: 6,
+        nom: 'PRPHTXIV & TSHIMBOMBO', 
+        photo: '/artists/julien.webp',
+        genre: 'House | Dub | Techno',
+        heure: '02h30',
+        origine: 'Poitiers - France',
+        description: ``
+      },
+
     ],
     j2: [
       {
@@ -68,7 +69,7 @@ export const artistesData = {
         nom: 'Sun Gazol', 
         photo: '/artists/sungazol.webp',
         genre: 'Pop Folk',
-        heure: '20h30 - 21h30',
+        heure: '20h00',
         origine: '',
         description: `Sun Gazol propose une pop folk organique et aérienne, où la folk se mêle aux atmosphères pop et psychédéliques.
                       Inspiré par des artistes comme Radiohead, Nick Drake, Half Moon Run ou Tame Impala, le groupe compose une musique immersive et envoûtante.
@@ -76,37 +77,39 @@ export const artistesData = {
       },
       {
         id: 8,
+        nom: "Yoga Teacher", 
+        photo: '/artists/yogateacher.webp',
+        genre: 'Cardio Psych Rock',
+        heure: '21h30',
+        origine: 'Bruxelles - Belgique',
+        description: `Situé dans les prémices de la sortie de leurs premiers enregistrements, le groupe bruxellois Yoga Teacher façonne un son que l’on pourrait qualifier d’indie psychédélique groovy et chargé en riffs — à la fois accrocheur et distordu — influencé par les parcours musicaux propres à chacun des quatre membres, ainsi que par leur passion commune pour un rock sans compromis.`
+      },
+
+      {
+        id: 9,
         nom: 'Stonks', 
         photo: '/artists/stonks.webp',
         genre: 'Post Punk | Post Rock | Alternative rock',
-        heure: '21h30 - 22h45',
+        heure: '23h00',
         origine: 'Bruxelles - Belgique',
         description: `Formé en 2021 par quatre musiciens attirés par la création originale et alternative, Stonks est un groupe indé Bruxellois. Ils sont inspiré par le rock, le jazz, le brouahahah désinvolte, les valses cassées, tous types de musiques pour cheveux longs, et surtout par la nouvelle scène post-punk anglaise, tirée au lasso par squid, shame et les viagra boys qui font parler d'eux depuis quelques années.`
       },
       {
-        id: 9,
+        id: 10,
         nom: "C'EST ELLE AIME",
         photo: '/artists/CLM.webp',
         genre: 'Mood House | Electro-Acid | Techno Industrial | Dub',
-        heure: '22h45 - 00h00',
+        heure: '00h30',
         origine: 'Poitiers - France',
         description: `Plongez dans l'univers cosmique et cyberpunk des protagonistes C'est Elle et Aime grace à leurs aventures retrouvés sous format audio. Genres musicaux diagnostiqués : mood house, electro-acid, techno indus/DUB.`
       },
-      {
-        id: 10,
-        nom: "TOOFOULK",
-        photo: '/artists/toofoulk.webp',
-        genre: 'Jungle | Drum and Bass | Neuro',
-        heure: '00h15 - 01h45',
-        origine: 'Poitiers - France',
-        description: ``
-      },
+
       {
         id: 11,
         nom: "Akae",
-        photo: '/artists/question.webp',
+        photo: '/artists/akae.webp',
         genre: 'Hard Techno',
-        heure: '01h45 - 02h45',
+        heure: '02h00',
         origine: 'Toulouse - France',
         description: ``
       },
@@ -115,7 +118,7 @@ export const artistesData = {
         nom: 'TWENTYHATE',
         photo: '/artists/twentyhate.webp',
         genre: 'Hard Music | Early Hardcore | Uptempo',
-        heure: '02h45 - 05h00',
+        heure: '03h30',
         origine: 'Toulouse - France',
         description: `TWENTYHATE vous entraîne dans un voyage sans concession : des atmosphères rugueuses de l'early hardcore aux déflagrations de kicks rythmés de l'uptempo.
                       Sur scène, chaque transition est un choc, chaque drop une explosion. Entre samples distordus, kicks surpuissants et breaks affûtés, il orchestre un véritable carnage auditif où l'on ne s'autorise aucun répit. Son objectif ? Faire découvrir au public des sonorités extrêmes tout en lâchant prise, et offrir une expérience live immersive, presque violente, mais toujours dans un esprit de joyeuseté.`
