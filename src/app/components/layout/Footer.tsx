9a24499015787fa5b6f3d33c46403f3f2b3c87f2
theo-hrd <theo.hrd02@gmail.com>
2025-05-20 20:14:33 +0200

    Enhance Footer component layout: update styling for improved responsiveness and add a new credit section to acknowledge the creators, enhancing overall user experience.

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -13,10 +13,15 @@ export default function ArtistList()
 
     return (   
         <footer className="bg-black py-8 border-t border-gray-800">
-            <div className="max-w-7xl mx-auto px-4 text-center">
+            <div className="max-w-7xl mx-auto px-4 text-center flex flex-col md:flex-row justify-center md:justify-between items-center gap-2 md:gap-0">
                 <p className="text-gray-400 text-sm">
                 &copy; {new Date().getFullYear()} Les Closurades Festival.
                 </p>
+                <div className="flex items-center gap-4">
+                    <p className="text-xs md:text-sm">
+                        Made by TWENTYHATE with ❤️
+                    </p>
+                </div>
             </div>
         </footer>
     );
